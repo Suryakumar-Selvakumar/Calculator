9b9ba18b2cff7c258d50853bc4cf76011d8dc957
Suryakumar Selvakumar <ssuryakumar2077@gmail.com>
2024-05-30 20:24:48 -0600

    Add HTML code and more JS Funcs

--- script.js
+++ script.js
@@ -13,3 +13,45 @@ function multiply(a, b) {
 function divide(a, b) {
   return a / b;
 }
+
+function mod(a, b) {
+  return Math.mod(a, b);
+}
+
+function changeSign(num) {
+  return (num *= -1);
+}
+
+function addDecimalPoint(num) {
+  return (num = num + ".");
+}
+
+function power(a, b) {
+  return a ** b;
+}
+
+let numberFirst, numberSecond, operator;
+
+function operate(op, num1, num2) {
+  let result;
+  switch (op) {
+    case "+":
+      result = add(num1, num2);
+      break;
+    case "-":
+      result = subtract(num1, num2);
+      break;
+    case "*":
+      result = multiply(num1, num2);
+      break;
+    case "/":
+      result = divide(num1, num2);
+      break;
+    case "%":
+      result = mod(num1, num2);
+      break;
+    case "**":
+      result = power(num1, num2);
+  }
+  return result;
+}
